bc9d3a9ac4a410ce3b68f294382e18f5e0a3f05f
Artem_Lakatun <alakatuni@mail.ru>
2024-04-28 11:39:17 +0300

    feat: add func delete user,add star

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,13 +1,24 @@
-import EmployersListItem from "../employees-list-item/employees-list-item";
-
 import './employees-list.css';
+import EmployeesListItem from "../employees-list-item/employees-list-item";
 
-const EmployeesList = () => {
+const EmployeesList = ({data, onDelete}) => {
+
+    const elem = data.map(item => {
+
+        const {id, ...itemProps} = item;
+
+        return (
+            < EmployeesListItem
+                key={id}
+                {...itemProps}
+                onDelete={() => onDelete(id)}
+            />
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployersListItem/>
-            <EmployersListItem/>
-            <EmployersListItem/>
+            {elem}
         </ul>
     );
 }
